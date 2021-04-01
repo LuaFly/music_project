86499f93e6d1f6844b77fb54de9dbf69c9fc3bff
Luafly <luasoestudos@yahoo.com>
2021-04-01 14:23:45 -0300

    menu desktop

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,61 @@
+<style>
+.header_menu {
+  text-align: end;
+  margin-right: 5%;
+}
+
+.header_menu ul li {
+  display: inline-block;
+  margin-left: 25px;
+  margin-top: 20px;
+}
+
+.header_menu ul li a {
+  color: #0378A6;
+  font-weight: bold;
+  letter-spacing: 0.1em;
+  font-size: 16px;
+  line-height: 20px;
+  padding: 10px 0;
+  text-decoration: none;
+  font-family:"Open Sans", sans-serif;
+}
+
+.header_menu ul li a:hover {
+  color: #023E73;
+}
+
+.header_menu ul li a.menu_ativo {
+  color: #023E73;
+}
+.signin{
+  border: 1px solid #04C4D9;
+  padding: 5px
+}
+</style>
+<header class="header">
+			<div class="container">
+				<nav class="header_menu ">
+        
+					<ul>
+						<li>
+							<a href="#" class="menu_ativo"> Home</a>
+						</li>
+						<li>
+							<a href="#"> Map</a>
+						</li>
+						<li>
+							<a href="#"> About us</a>
+						</li>
+						<li>
+							<a href="#"> <span class="signin"> Sign in <span></a>
+						</li>
+					</ul>
+				</nav>
+        <div class="img">
+        <a href="#" >
+					<img src="img/logo.png" width="150px" alt="logo-bikcraft">
+				</a>
+        </div>
+			</div>
+</header>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Document</title>
+  <style>
+    .box {
+      background: #EAFBFE;
+      overflow-y: auto;
+      overflow-x: hidden;
+      border-radius:10px;
+      box-shadow: 4px 3px 13px 1px rgba(47,105,222,0.56);
+      -webkit-box-shadow: 4px 3px 13px 1px rgba(47,105,222,0.56);
+      -moz-box-shadow: 4px 3px 13px 1px rgba(47,105,222,0.56);
+    }
+        
+  </style>
+</head>
+<body>
+<div class="row d-flex justify-content-center">
+  <div class="light-blue box col-10 ">
+    @include('components/menu')
+  </div>
+</div>
+</body>
+</html>
